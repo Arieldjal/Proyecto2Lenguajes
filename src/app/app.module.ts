0fc8bf0a15706184f765272435ca73f9135246a8
danii230 <37677168+danii230@users.noreply.github.com>
2020-07-01 23:05:30 -0600

    Daniela
    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseAddComponent } from './course-add/course-add.component';
+import { CourseUpdateComponent } from './course-update/course-update.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
 const appRoutes: Routes = [
   {
@@ -18,17 +22,48 @@ const appRoutes: Routes = [
     path: '',
     redirectTo: '/login',
     pathMatch: 'full'
+  },
+  {
+    path: 'courses',
+    component: CourseListComponent,
+    data: { title: 'Course List' }
+  },
+  {
+    path: 'course-add',
+    component: CourseAddComponent,
+    data: { title: 'Course Add' }
+  },
+  {
+    path: 'course-details/:id',
+    component: CourseDetailsComponent,
+    data: { title: 'Course Details' }
+  },
+  {
+    path: 'course-update/:id',
+    component: CourseUpdateComponent,
+    data: { title: 'Student Update' }
+  },
+  
+  { 
+    path: '',
+    redirectTo: '/courses',
+    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CourseListComponent,
+    CourseAddComponent,
+    CourseUpdateComponent,
+    CourseDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,14 +1,74 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http'; 
-import { Observable, of } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {Observable, of, pipe} from 'rxjs';
+import {map, catchError, tap} from 'rxjs/operators';
 
-const endpoint = '';
+const endpoint = 'http://127.0.0.1:8181/api/';
+const httpOptions = {
+  headers : new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getCourses(): Observable<any> {
+    return this.http.get(endpoint + 'courses').pipe(
+      map(this.extractData),
+      catchError(this.handleError<any>('getCourses'))
+      );
+  }
+  
+  getCourse(id): Observable<any> {
+    console.log(id);
+    
+    return this.http.get(endpoint + 'courses/' + id).pipe(
+      map(this.extractData),
+      catchError(this.handleError<any>('getCourse'))
+      );
+  }
+
+  addCourse(course): Observable<any> {
+    return this.http.post<any>(endpoint + 'courses/add', JSON.stringify(course), httpOptions).pipe(
+      tap((course) => console.log('added course')),
+      catchError(this.handleError<any>('addCourse'))
+    );
+  }
+
+  deleteCourse (id): Observable<any> {
+    return this.http.delete<any>(endpoint + 'courses/delete/' + id, httpOptions).pipe(
+      tap(_ => console.log(`deleted course id=${id}`)),
+      catchError(this.handleError<any>('deleteCourse'))
+    );
+  }
+
+  updateCourse (id, course): Observable<any> {
+    console.log(course);
+    return this.http.put(endpoint + 'courses/update/' + id, JSON.stringify(course), httpOptions).pipe(
+      tap((course) => console.log('updated course')),
+      catchError(this.handleError<any>('updateCourse'))
+    );
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
 }
+
